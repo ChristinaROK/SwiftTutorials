929ff7b478369ca0af04850a15ff754375106a23
Christina <myway2027@gmail.com>
2021-07-01 23:58:35 +0900

    new

---
+++ Swift101/Swift101/InitEnum.swift
@@ -0,0 +1,20 @@
+//
+//  InitEnum.swift
+//  Swift101
+//
+//  Created by ssj on 2021/07/01.
+//
+
+import SwiftUI
+
+struct InitEnum: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InitEnum_Previews: PreviewProvider {
+    static var previews: some View {
+        InitEnum()
+    }
+}
