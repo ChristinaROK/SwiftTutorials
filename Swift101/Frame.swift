759d4565b19d83a58211f724dfb69db1acd4475a
Christina <myway2027@gmail.com>
2021-07-08 23:52:44 +0900

    new

---
+++ Swift101/Frame.swift
@@ -0,0 +1,20 @@
+//
+//  Frame.swift
+//  Swift101
+//
+//  Created by ssj on 2021/07/08.
+//
+
+import SwiftUI
+
+struct Frame: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Frame_Previews: PreviewProvider {
+    static var previews: some View {
+        Frame()
+    }
+}
